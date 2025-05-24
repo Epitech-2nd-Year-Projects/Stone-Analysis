057dd09dd4b9f7ebc70684986e4ca27f31038558
Yanis Kernoua <yanis.kernoua@epitech.eu>
2025-05-24 18:19:01 +0200

    feat(dft): AnalysisResult struct for frequency analysis results

--- internal/analyze/types.go
+++ internal/analyze/types.go
@@ -4,3 +4,10 @@ type FreqMag struct {
 	Freq float64
 	Mag  float64
 }
+
+type AnalysisResult struct {
+	TopFrequencies []FreqMag
+	SampleRate     float64
+	SampleCount    int
+	WindowUsed     bool
+}
